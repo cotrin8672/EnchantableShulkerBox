069333a33c90ff219190ab3c2f151710ab7a6c1e
cotrin8672 <gummy8672@gmail.com>
2024-12-21 08:04:00 +0900

    fix: add replace property

--- common/src/main/java/io/github/cotrin8672/enchantableshulkerbox/registry/ModTags.kt
+++ common/src/main/java/io/github/cotrin8672/enchantableshulkerbox/registry/ModTags.kt
@@ -4,11 +4,10 @@ import io.github.cotrin8672.enchantableshulkerbox.EnchantableShulkerBox
 import net.minecraft.item.Item
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
-import net.minecraft.util.Identifier
 
 object ModTags {
     @JvmStatic
-    val SHULKER_BOXES: TagKey<Item> = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "shulker_boxes"))
+    val SHULKER_BOXES: TagKey<Item> = bind("shulker_boxes")
 
     private fun bind(name: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, EnchantableShulkerBox.of(name))
 }
